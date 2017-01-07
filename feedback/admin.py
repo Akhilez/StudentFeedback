c85ad08cb864540588ebb60d21456f5cdf441580
Akhilez <akhild18@yahoo.com>
2017-01-07 22:43:30 +0530

    Akhil-models done 2

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from feedback.models import *
 
 # Register your models here.
+
+
+admin.site.register(Classes)
+admin.site.register(Subject)
+admin.site.register(Faculty)
+admin.site.register(ClassFacSub)
+admin.site.register(Student)
--- feedback/models.py
+++ feedback/models.py
@@ -1,12 +1,16 @@
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MinValueValidator, MaxValueValidator, validate_comma_separated_integer_list
 from django.db import models
 
 # Create your models here.
 
 
+MAX_QUESTIONS = 20
+
+
 class Classes(models.Model):
-    class_id = models.IntegerField(primary_key=True)
+    class_id = models.AutoField(primary_key=True)
     year = models.IntegerField(
         validators=[MaxValueValidator(4), MinValueValidator(1)] #use IntegerRangeField when admin enters the years
     )
@@ -25,7 +29,7 @@ class Subject(models.Model):
 
 
 class ClassFacSub(models.Model):
-    relation_id = models.IntegerField(primary_key=True)
+    relation_id = models.AutoField(primary_key=True)
     class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
     faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
@@ -37,14 +41,14 @@ class Student(models.Model):
 
 
 class Initiation(models.Model):
-    initiation_id = models.IntegerField(primary_key=True)
+    initiation_id = models.AutoField(primary_key=True)
     timestamp = models.DateTimeField()
     initiated_by = models.ForeignKey(User, on_delete=models.CASCADE)
     class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
 
 
 class Session(models.Model):
-    session_id = models.IntegerField(primary_key=True)
+    session_id = models.AutoField(primary_key=True)
     timestamp = models.DateTimeField()
     initiation_id = models.ForeignKey(Initiation, on_delete=models.CASCADE)
     taken_by = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -54,17 +58,37 @@ class Session(models.Model):
 class Attendance(models.Model):
     session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
     student_id = models.ForeignKey(Student)
-    mac_address = models.CharField(max_length=20)
+    mac_address = models.GenericIPAddressField()#models.CharField(max_length=20)
 
 
 class Notes(models.Model):
-    note_id = models.IntegerField(primary_key=True)
-    note = models.CharField(max_length=350)
+    note_id = models.AutoField(primary_key=True)
+    note = models.TextField()
+
+
+class FdbkQuestions(models.Model):
+    question_id = models.AutoField(primary_key=True, validators=[MaxValueValidator(MAX_QUESTIONS), MinValueValidator(1)])
+    question = models.TextField()
 
 
 class Feedback(models.Model):
     session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
     relation_id = models.ForeignKey(ClassFacSub, on_delete=models.CASCADE)
+
+    ratings = models.CharField(
+        validators=[validate_comma_separated_integer_list],
+        max_length=MAX_QUESTIONS**2-1
+    )
+
+    #ratings = models.CommaSeparatedIntegerField(max_length=20)  #Deprecated way to use it
+
+    '''  #this works only with postgresql
+    questions = ArrayField(
+        models.IntegerField(validators=[MaxValueValidator(5)]),
+        size=MAX_QUESTIONS,
+    )
+    '''
+    '''  #this is a very bad way
     question_1 = models.IntegerField(validators=[MaxValueValidator(5)])
     question_2 = models.IntegerField(validators=[MaxValueValidator(5)])
     question_3 = models.IntegerField(validators=[MaxValueValidator(5)])
@@ -85,10 +109,6 @@ class Feedback(models.Model):
     question_18 = models.IntegerField(validators=[MaxValueValidator(5)])
     question_19 = models.IntegerField(validators=[MaxValueValidator(5)])
     question_20 = models.IntegerField(validators=[MaxValueValidator(5)])
+    '''
     remarks = models.ForeignKey(Notes, on_delete=models.CASCADE)
-    idk = models.ForeignKey(Notes, on_delete=models.CASCADE)
-
-
-class FdbkQuestions(models.Model):
-    question_id = models.IntegerField(validators=[MaxValueValidator(20)], primary_key=True)
-    question = models.CharField(max_length=200)
+    #idk = models.ForeignKey(Notes, on_delete=models.CASCADE)
