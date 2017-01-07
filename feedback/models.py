aeef223e4a89f6421db20851262c84e6883b23ca
Akhilez <akhild18@yahoo.com>
2017-01-07 21:03:10 +0530

    Akhil-models done

--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
@@ -29,3 +30,65 @@ class ClassFacSub(models.Model):
     faculty_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     subject_id = models.ForeignKey(Subject, on_delete=models.CASCADE)
 
+
+class Student(models.Model):
+    hallticket_no = models.CharField(max_length=10, primary_key=True)
+    class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
+
+
+class Initiation(models.Model):
+    initiation_id = models.IntegerField(primary_key=True)
+    timestamp = models.DateTimeField()
+    initiated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
+
+
+class Session(models.Model):
+    session_id = models.IntegerField(primary_key=True)
+    timestamp = models.DateTimeField()
+    initiation_id = models.ForeignKey(Initiation, on_delete=models.CASCADE)
+    taken_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=5, unique=True)
+
+
+class Attendance(models.Model):
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student)
+    mac_address = models.CharField(max_length=20)
+
+
+class Notes(models.Model):
+    note_id = models.IntegerField(primary_key=True)
+    note = models.CharField(max_length=350)
+
+
+class Feedback(models.Model):
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    relation_id = models.ForeignKey(ClassFacSub, on_delete=models.CASCADE)
+    question_1 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_2 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_3 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_4 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_5 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_6 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_7 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_8 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_9 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_10 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_11 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_12 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_13 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_14 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_15 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_16 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_17 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_18 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_19 = models.IntegerField(validators=[MaxValueValidator(5)])
+    question_20 = models.IntegerField(validators=[MaxValueValidator(5)])
+    remarks = models.ForeignKey(Notes, on_delete=models.CASCADE)
+    idk = models.ForeignKey(Notes, on_delete=models.CASCADE)
+
+
+class FdbkQuestions(models.Model):
+    question_id = models.IntegerField(validators=[MaxValueValidator(20)], primary_key=True)
+    question = models.CharField(max_length=200)
--- feedback/tests.py
+++ feedback/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+a = [1,2,3,4,5,6,7,8,9,0,-1]
+n=11
+m=3
+for i in range(0, n+1, m):  #i=i+m
+    for j in range(m+i,i,-1):
+        if j >= n: break
+        print(a[j-1])
+for i in range(n-1, n-n%m-1, -1):
+    print(a[i])
