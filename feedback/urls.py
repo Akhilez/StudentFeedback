d4f720286bde3af15c06a778d3ad08373ce8968b
Akhilez <akhild18@yahoo.com>
2017-01-10 00:07:23 +0530

    Akhil-dynamin drop down3

--- feedback/urls.py
+++ feedback/urls.py
@@ -9,8 +9,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', views.login_redirect, name="login_redirect"),
     url(r'^initiate/()()()$', views.initiate, name="initiate"),
-    url(r'^initiate/([1-4])/()()$', views.initiate, name="initiate"),
-    url(r'^initiate/([1-4])/(\w+)/()$', views.initiate, name="initiate"),
-    url(r'^initiate/([1-4])/(\w+)/(\w+)/$', views.initiate, name="initiate"),
+    url(r'^initiate/([0-9])/()()$', views.initiate, name="initiate"),
+    url(r'^initiate/([0-9])/(\w+)/()$', views.initiate, name="initiate"),
+    url(r'^initiate/([0-9])/(\w+)/(\w+)/$', views.initiate, name="initiate"),
     url(r'^conduct/$', views.conduct, name="conduct"),
 ]
