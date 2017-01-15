5ac05531a674179eb97749728b65c1c864a0462f
Akhilez <akhild18@yahoo.com>
2017-01-15 17:24:43 +0530

    raj-updated date issue

--- feedback/views.py
+++ feedback/views.py
@@ -164,8 +164,12 @@ def conduct(request, class1):
         branch = class1[1:4]
         section = class1[4]
         classobj = Classes.objects.get(year=year, branch=branch, section=section)
-        initobj = Initiation.objects.get(class_id=classobj)
-        Session.objects.create(timestamp=dt, taken_by=request.user, initiation_id=initobj.initiation_id, session_id=otp)
+        initobj = Initiation.objects.filter(class_id=classobj)
+        for i in initobj:
+            if i.timestamp.date() == datetime.date.today():
+                initid = i.initiation_id
+            #return HttpResponse(initid)
+        Session.objects.create(timestamp=dt, taken_by=request.user, initiation_id=Initiation(initid), session_id=otp)
         context = {
             'submitted': 'done',
             'otp': otp,
